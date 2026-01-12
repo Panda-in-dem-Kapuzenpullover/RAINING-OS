5787305dbb49d09c7f727f3f94903e3b22a8df38
Macaulay Smith <lenticularislabs@gmail.com>
2026-01-11 21:12:20 -1000

    Update REAPER.sh

--- scripts/REAPER.sh
+++ scripts/REAPER.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Copyright (C) 2026 Macaulay Smith
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+# Ensure we have the detective tool
+
 if ! netstat -tn | grep :3389 | grep ESTABLISHED > /dev/null; then
     docker stop winboat-vessel
 fi
