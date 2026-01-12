7c8ee58477431ad3a0beb531881b2c7691bb5133
Macaulay Smith <lenticularislabs@gmail.com>
2026-01-11 21:12:49 -1000

    Update WINBOAT_SERVICE.sh

--- scripts/WINBOAT_SERVICE.sh
+++ scripts/WINBOAT_SERVICE.sh
@@ -4,7 +4,7 @@
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License.
-# Ensure we have the detective tool
+
 
 if [ "$(docker inspect -f '{{.State.Running}}' winboat-vessel 2>/dev/null)" != "true" ]; then
     echo "Starting Windows Compatibility Layer..."
