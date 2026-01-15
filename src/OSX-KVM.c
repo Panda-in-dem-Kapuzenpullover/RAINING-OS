ab504670be9f80bd4101ed8f9abfe4460f40ca11
Macaulay Smith <lenticularislabs@gmail.com>
2026-01-14 21:56:51 -1000

    Update OSX-KVM.c

--- src/OSX-KVM.c
+++ src/OSX-KVM.c
@@ -2,7 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
+/*
+ * RAINING-OS Secure Update System
+ * Copyright (C) 2026 Macaulay Smith
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
 void setup_wizard() {
     char decision;
     printf("Would you like to continue? [Y/N] ");
